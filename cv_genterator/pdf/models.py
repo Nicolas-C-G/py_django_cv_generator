b85766d88666ef18ad874c2d0de2c24ec417c722
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-05-26 23:58:35 -0400

    form created

--- cv_genterator/pdf/models.py
+++ cv_genterator/pdf/models.py
@@ -11,5 +11,5 @@ class Profile(models.Model):
     degree        = models.CharField(max_length=200)
     school        = models.CharField(max_length=200)
     university    = models.CharField(max_length=200)
-    previous_work = models.CharField(max_length=2000)
-    skills        = models.CharField(max_length=2000)
+    previous_work = models.TextField(max_length=2000)
+    skills        = models.TextField(max_length=2000)
