fa9ab17ac597570692869a3b20f98567ca4dd44b
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-05-27 20:43:30 -0400

    basic cv generator done

--- cv_genterator/pdf/views.py
+++ cv_genterator/pdf/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Profile
+import pdfkit
+from django.http import HttpResponse
+from django.template import loader
+import io
+
 # Create your views here.
 
 
@@ -20,3 +25,23 @@ def accept(request):
         profile.save()
 
     return render(request, 'pdf/accept.html')
+
+def resume(request,id):
+    user_profile = Profile.objects.get(pk=id)
+    template     = loader.get_template('pdf/resume.html')
+    html         = template.render({'user_profile':user_profile})
+    options      = {
+        'page-size':'Letter',
+        'encoding': 'UTF-8',
+    }
+
+    pdfl = pdfkit.from_string(html,False,options)
+    response = HttpResponse(pdfl,content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment'
+    response['filename'] = "resume.pdf"
+
+    return response
+
+def list(request):
+    profiles = Profile.objects.all()
+    return render(request,'pdf/list.html',{'profiles':profiles})
